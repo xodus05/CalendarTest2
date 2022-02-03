edace35e106a8dd638c2bc59098d2c2e974803ae
xodus05 <s2121@e-mirim.hs.kr>
2022-02-03 17:57:54 +0900

    일기 파일

--- app/src/main/java/com/example/calendartest2/MainActivity.java
+++ app/src/main/java/com/example/calendartest2/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.calendartest2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,5 +39,59 @@ public class MainActivity extends AppCompatActivity {
        int cYear = c.get(Calendar.YEAR);
        int cMonth = c.get(Calendar.MONTH);
        int cDay = c.get(Calendar.DAY_OF_MONTH);
+
+       //첫 시작 시에는 오늘 날짜 일기 읽어주기
+       checkdDay(cYear, cMonth, cDay);
+
+       // datePick 기능 만들기
+       // datePicker.init(연도, 달, 일)
+       datePicker.init(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), new DatePicker.OnDateChangedListener() {
+           @Override
+           public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+               //이미 선택한 날짜에 일기가 있는지 없는지 체그해야할 시간이다.
+               checkedDay(year, monthOfYear, dayOfMonth);
+           }
+       });
+
+       //저장/수정 버튼 누르면 실행되는 리스너
+       btnSave.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //fileName을 넣고 저장 시키는 메소드를 호출
+               saveDiary(fileName);
+           }
+       });
     }
+
+    //일기 파일 읽기
+    private void checkdDay(int year, int monthOfyear, int dayOfMonth) {
+
+       //받은 날짜로 날짜 보여주는
+        viewDatePick.setText(year + " - " + monthOfyear + " - " + dayOfMonth);
+
+       // 파일 이름을 만들어준다.
+       fileName = year +"" + monthOfyear + "" + dayOfMonth + ".txt";
+
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(fileName);
+
+            byte[] fileData = new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+
+            String str = new String(fileData, "EUC-KR");
+            Toast.makeText(getApplicationContext(), "일기 써둔 날", Toast.LENGTH_SHORT).show();
+            edtDiary.setText(str);
+            btnSave.setText("수정하기");
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "일기 없는 날", Toast.LENGTH_SHORT).show();
+            edtDiary.setText("");
+            btnSave.setText("새 일기 저장");
+            e.printStackTrace();
+        }
+    }
+
+    //일기 저장하는 메소드
 }
